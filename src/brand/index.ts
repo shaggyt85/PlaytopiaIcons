afe7678a5270c5d15d9efeac6214a62d255a52f4
Robert Medina <csoperativo@gmail.com>
2026-04-04 00:10:54 +0200

    feat: add LogoIsotipo brand asset

--- src/brand/index.ts
+++ src/brand/index.ts
@@ -3,3 +3,6 @@ export type { LogoHorizontalProps } from './LogoHorizontal';
 
 export { LogoHorizontalSmall } from './LogoHorizontalSmall';
 export type { LogoHorizontalSmallProps } from './LogoHorizontalSmall';
+
+export { LogoIsotipo } from './LogoIsotipo';
+export type { LogoIsotipoProps } from './LogoIsotipo';
